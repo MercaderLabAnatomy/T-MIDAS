d2909881324175f04472d9163b4dc00b23fc04e4
Marco Meer <marco.meer@pm.me>
2024-01-16 15:14:43 +0100

    intermediate solution for count instances per ROI

--- scripts/count_instances_per_ROI.py
+++ scripts/count_instances_per_ROI.py
@@ -10,7 +10,8 @@ import tifffile as tf
 import napari_simpleitk_image_processing as nsitk  # version 0.4.5
 import pyclesperanto_prototype as cle
 import napari_segment_blobs_and_things_with_membranes as nsbatwm
-
+import re
+from skimage.measure import regionprops
 
 # Argument Parsing
 parser = argparse.ArgumentParser(description="Input: Folder with all label images (ROIs and instance segmentations).")
@@ -18,19 +19,19 @@ parser.add_argument("--input", type=str, required=True, help="Path to input labe
 args = parser.parse_args()
 
 
-def get_area(ROI):
-    area = cle.sum_of_all_pixels(ROI)
-    return area
+# def get_area(ROI):
+#     area = cle.sum_of_all_pixels(ROI)
+#     return area
 
-def get_circularity(ROI):
-    region = measure.regionprops(ROI)
-    circularity = 4.0 * np.pi * region.area / (region.perimeter ** 2.0) 
-    return circularity
+# def get_circularity(ROI):
+#     region = regionprops(ROI)
+#     circularity = 4.0 * np.pi * region.area / (region.perimeter ** 2.0) 
+#     return circularity
 
-def get_AR(ROI):
-    region = measure.regionprops(ROI)
-    AR = region.major_axis_length / region.minor_axis_length
-    return AR
+# def get_AR(ROI):
+#     region = regionprops(ROI)
+#     AR = region.major_axis_length / region.minor_axis_length
+#     return AR
 
 def counter(ROI,instances):
     instances_inside_ROI = cle.binary_and(ROI, instances)
@@ -43,21 +44,21 @@ def counter(ROI,instances):
 def ROI2CSV(image_path):
 
     # load label images
-    ventricle_wo_injury = imread(image_path.replace(".tif", " - _ventricle_wo_injury.tif"))
-    injury = imread(image_path.replace(".tif", " - _injury.tif"))
-    epicardium = imread(image_path.replace(".tif", " - _epicardium.tif"))
-    border_zone = imread(image_path.replace(".tif", " - _border_zone.tif"))
-    instances = imread(image_path.replace(".tif", " - _labels.tif"))
+    ventricle_wo_injury = imread(image_path.replace(".tif", "_ventricle_wo_injury.tif"))
+    injury = imread(image_path.replace(".tif", "_injury.tif"))
+    epicardium = imread(image_path.replace(".tif", "_epicardium.tif"))
+    border_zone = imread(image_path.replace(".tif", "_border_zone.tif"))
+    instances = imread(image_path.replace(".tif", "_labels.tif").replace("FITC", "CY5"))
 
     # create CSV file
     filename = image_path.replace(".tif", ".csv")
     with open(filename, 'w') as f:
         writer = csv.writer(f)
-        writer.writerow(["ROI", "instances","ROI_area (pixels squared)","circularity","aspect_ratio"])
-        writer.writerow(["ventricle_wo_injury", counter(ventricle_wo_injury,instances), get_area(ventricle_wo_injury), get_circularity(ventricle_wo_injury), get_AR(ventricle_wo_injury)])
-        writer.writerow(["injury", counter(injury,instances), get_area(injury), get_circularity(injury), get_AR(injury)])
-        writer.writerow(["epicardium", counter(epicardium,instances), get_area(epicardium), get_circularity(epicardium), get_AR(epicardium)])
-        writer.writerow(["border_zone", counter(border_zone,instances), get_area(border_zone), get_circularity(border_zone), get_AR(border_zone)])
+        writer.writerow(["ROI", "instances"])#,"ROI_area (pixels squared)","circularity","aspect_ratio"])
+        writer.writerow(["ventricle_wo_injury", counter(ventricle_wo_injury,instances)])#, get_area(ventricle_wo_injury), get_circularity(ventricle_wo_injury), get_AR(ventricle_wo_injury)])
+        writer.writerow(["injury", counter(injury,instances)])#, get_area(injury), get_circularity(injury), get_AR(injury)])
+        writer.writerow(["epicardium", counter(epicardium,instances)])#, get_area(epicardium), get_circularity(epicardium), get_AR(epicardium)])
+        writer.writerow(["border_zone", counter(border_zone,instances)])#, get_area(border_zone), get_circularity(border_zone), get_AR(border_zone)])
 
 
 
@@ -65,14 +66,17 @@ def ROI2CSV(image_path):
 filenames = os.listdir(args.input)
 # filter out files that don't end with .tif
 
+filenames = os.listdir("/mnt/TEST/")
+filenames = [s for s in filenames if "CY5" not in s]
 new_filenames = []
 
 for filename in filenames:
     if filename.endswith(".tif"):
-        index = filename.rfind("-")
-        new_filename = filename[:index] + ".tif"
-        new_filenames.append(new_filename)
-
+        match = re.search(r'.+_roi_\d{2,3}', filename)
+        if match:
+            index = match.group(0)
+            new_filename = filename[:match.end()] + ".tif"
+            new_filenames.append(new_filename)
 
 # remove duplicates
 new_filenames = list(dict.fromkeys(new_filenames))
