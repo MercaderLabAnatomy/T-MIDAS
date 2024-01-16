6cb73411b3bff414ec2bea9071575554354d3456
Marco Meer <marco.meer@pm.me>
2024-01-16 13:52:46 +0100

    cleaned and improved speed of created ventricle ROIs script

--- scripts/create_ventricle_ROIs.py
+++ scripts/create_ventricle_ROIs.py
@@ -8,10 +8,7 @@ from skimage.io import imread
 import tifffile as tf
 import napari_simpleitk_image_processing as nsitk  # version 0.4.5
 import pyclesperanto_prototype as cle
-import napari_segment_blobs_and_things_with_membranes as nsbatwm
 from skimage.measure import regionprops
-
-
 
 # Argument Parsing
 parser = argparse.ArgumentParser(description="Input: Folder with label images containing masks of intact ventricle and injury regions.")
@@ -21,7 +18,7 @@ args = parser.parse_args()
 
 EPICARDIUM_DIAMETER_UM = 15.0
 EPICARDIUM_ADDITIONAL_DIAMETER_UM = 10.0
-PIXEL_RESOLUTION = 0.23 # Slidescanner Hamamatsu S60 at 40x
+PIXEL_RESOLUTION = args.pixel_resolution # Slidescanner Hamamatsu S60 at 40x is 0.23 um/px
 EPICARDIUM_DIAMETER_PX = EPICARDIUM_DIAMETER_UM / PIXEL_RESOLUTION
 EPICARDIUM_ADDITIONAL_DIAMETER_PX = EPICARDIUM_ADDITIONAL_DIAMETER_UM / PIXEL_RESOLUTION
 INTACT_VENTRICLE_REGION_LABEL_ID = 1
@@ -32,157 +29,126 @@ SMALL_LABELS_THRESHOLD = 10000.0 # square pixels
 
 # Define utility functions
 
+def gpu_processing(array):
+    try:
+        label_image = cle.push(array)
+        label_image = cle.merge_touching_labels(label_image)
+        label_image = nsitk.binary_fill_holes(label_image)
+        label_image = cle.connected_components_labeling_box(label_image)
+        label_image = cle.pull(label_image)
+        
+        return label_image
+    except Exception as e:
+        print(f"Error processing {image}: {str(e)} on the gpu.")
+        return None
 
-def get_ventricle(image_path):
+
+def get_ventricle(image):
     try:
 
-        image = imread(image_path)        
-        ventricle = cle.merge_touching_labels(image)
-        ventricle = nsitk.binary_fill_holes(np.array(ventricle))
-        ventricle = cle.connected_components_labeling_box(ventricle)
-        ventricle = cle.exclude_small_labels(ventricle, None, SMALL_LABELS_THRESHOLD)
-        ventricle = np.array(ventricle, dtype=np.uint64)
+        ventricle = gpu_processing(image)
+        label_props = regionprops(ventricle)
+        largest_label = max(label_props, key=lambda region: region.area)
+        print(f"Ventricle label: {largest_label.label}")
+        ventricle = np.where(ventricle == largest_label.label, ventricle, 0)
+        ventricle = ventricle.astype(np.int64)
 
         return ventricle
     
     except Exception as e:
-        print(f"Error processing {image_path}: {str(e)} while getting ventricle.")
+        print(f"Error processing {image}: {str(e)} while getting ventricle.")
         return None
 
-
-
-# image_path = "MF20/wt_7dpi_5_2 - 3 - CY5_8bit_labels.tif"
-
-
-# import napari
-# if 'viewer' not in globals():
-#     viewer = napari.Viewer()
-
-# ventricle = get_ventricle(image_path)
-# viewer.add_labels(ventricle)
-# tf.imwrite(image_path+ "_ventricle.tif", ventricle,compression='zlib')
-
-
-def get_ventricle_wo_injury(image_path):
+def get_ventricle_wo_injury(image):
     try:
-
-        image = imread(image_path)     
+  
         ventricle_wo_injury = np.copy(image)
         ventricle_wo_injury[ventricle_wo_injury == INJURY_LABEL_ID] = 0
-        ventricle_wo_injury = nsitk.binary_fill_holes(ventricle_wo_injury)
-        ventricle_wo_injury = cle.connected_components_labeling_box(ventricle_wo_injury)
+        ventricle_wo_injury = gpu_processing(ventricle_wo_injury)
         label_props = regionprops(ventricle_wo_injury)
         largest_label = max(label_props, key=lambda region: region.area)
-        ventricle_wo_injury = np.where(ventricle_wo_injury == largest_label, ventricle_wo_injury, 0)
-        ventricle_wo_injury = np.array(ventricle_wo_injury, dtype=np.uint64)
+        ventricle_wo_injury = np.where(ventricle_wo_injury == largest_label.label, ventricle_wo_injury, 0)
+        ventricle_wo_injury = ventricle_wo_injury.astype(np.int64)
 
         return ventricle_wo_injury
     
     except Exception as e:
-        print(f"Error processing {image_path}: {str(e)} while getting ventricle without injury.")
+        print(f"Error processing {image}: {str(e)} while getting ventricle without injury.")
         return None
 
 
-def get_injury(image_path):
+
+def get_injury(ventricle, ventricle_wo_injury):
     try:
 
-        image = imread(image_path)
-        injury = np.copy(image)
-        injury[injury == INTACT_VENTRICLE_REGION_LABEL_ID] = 0
-        injury = nsitk.binary_fill_holes(injury)
-        injury = cle.connected_components_labeling_box(injury)
-        label_props = regionprops(injury)
-        largest_label = max(label_props, key=lambda region: region.area)
-        injury = np.where(injury == largest_label, injury, 0)
-        injury = np.array(injury, dtype=np.uint64)
+        # subtract ventricle_wo_injury from ventricle to get injury
+        ventricle = cle.push(ventricle)
+        ventricle_wo_injury = cle.push(ventricle_wo_injury)
+        injury = cle.binary_subtract(ventricle, ventricle_wo_injury)
+        injury = cle.pull(injury)
+        injury = injury.astype(np.int64)
 
         return injury
     except Exception as e:
-        print(f"Error processing {image_path}: {str(e)}  while getting injury.")
+        print(f"Error processing {image}: {str(e)}  while getting injury.")
         return None
 
-# injury = get_injury(image_path)
 
-# tf.imwrite(image_path+ "_injury.tif", injury)
 
-def get_epicardium(image_path):
+def get_epicardium(ventricle):
     try:
 
-        image = imread(image_path)
-        
-        ventricle = cle.merge_touching_labels(image)
-        ventricle = cle.exclude_small_labels(ventricle, None, SMALL_LABELS_THRESHOLD)
-        ventricle = nsitk.binary_fill_holes(np.array(ventricle))
-        #viewer.add_labels(ventricle)
-        not_ventricle = nsbatwm.binary_invert(ventricle)
-        not_ventricle_expanded = nsbatwm.expand_labels(not_ventricle, EPICARDIUM_DIAMETER_PX)
-        #viewer.add_labels(not_ventricle)
-        #viewer.add_labels(not_ventricle_expanded)
-        epicardium = cle.binary_and(not_ventricle_expanded, ventricle)
-        ventricle = cle.binary_subtract(ventricle, epicardium)
-        epicardium = nsbatwm.expand_labels(epicardium, EPICARDIUM_ADDITIONAL_DIAMETER_PX)
+        ventricle = cle.push(ventricle)
+        not_ventricle = cle.binary_not(ventricle)
+        not_ventricle_dilated = cle.dilate_labels(not_ventricle, None, EPICARDIUM_DIAMETER_PX)
+        epicardium = cle.binary_and(not_ventricle_dilated, ventricle)
+        epicardium = cle.dilate_labels(epicardium, None, EPICARDIUM_ADDITIONAL_DIAMETER_PX)
         epicardium = cle.binary_subtract(epicardium, ventricle)
         epicardium = cle.connected_components_labeling_box(epicardium)
+        epicardium = cle.pull(epicardium)
         label_props = regionprops(epicardium)
         largest_label = max(label_props, key=lambda region: region.area)
-        epicardium = np.where(epicardium == largest_label, epicardium, 0)
-        #viewer.add_labels(epicardium)
-        # convert epicardium to ndarray
-        epicardium = np.array(epicardium, dtype=np.uint64)
+        epicardium = np.where(epicardium == largest_label.label, epicardium, 0)
+        epicardium = epicardium.astype(np.int64)
 
         return epicardium
     
     except Exception as e:
-        print(f"Error processing {image_path}: {str(e)} while getting epicardium.")
+        print(f"Error processing {image}: {str(e)} while getting epicardium.")
         return None
 
-# epicardium = get_epicardium(image_path)
 
-# tf.imwrite(image_path+ "_epicardium.tif", epicardium,compression='zlib')
-
-# imread(image_path+ "_epicardium.tif")
-
-def get_border_zone(image_path):
+def get_border_zone(injury, ventricle_wo_injury):
     try:
 
-        image = imread(image_path)      
-        ventricle = cle.merge_touching_labels(image)
-        ventricle = cle.exclude_small_labels(ventricle, None, SMALL_LABELS_THRESHOLD)
-        injury = np.copy(image)
-        injury[injury == INTACT_VENTRICLE_REGION_LABEL_ID] = 0
-        injury = nsitk.binary_fill_holes(injury)
-        injury = cle.exclude_small_labels(injury, None, SMALL_LABELS_THRESHOLD)
-        injury_expanded = nsbatwm.expand_labels(injury, BORDER_ZONE_DIAMETER_PX)
-        ventricle_wo_injury = np.copy(image)
-        ventricle_wo_injury[ventricle_wo_injury == INJURY_LABEL_ID] = 0
-        ventricle_wo_injury = nsitk.binary_fill_holes(ventricle_wo_injury)
-        ventricle_wo_injury = cle.connected_components_labeling_box(ventricle_wo_injury)
-        ventricle_wo_injury = cle.exclude_small_labels(ventricle_wo_injury, None, SMALL_LABELS_THRESHOLD)
-        border_zone = cle.binary_and(ventricle_wo_injury, injury_expanded)
-        border_zone = cle.connected_components_labeling_box(border_zone)
-        label_props = regionprops(border_zone)
-        largest_label = max(label_props, key=lambda region: region.area)
-        border_zone = np.where(border_zone == largest_label, border_zone, 0)
-        border_zone = np.array(border_zone, dtype=np.uint64)
+        injury = cle.push(injury)
+        ventricle_wo_injury = cle.push(ventricle_wo_injury)
+        injury_dilated = cle.dilate_labels(injury, None, BORDER_ZONE_DIAMETER_PX)
+        border_zone = cle.binary_and(injury_dilated, ventricle_wo_injury)
+        border_zone = cle.pull(border_zone)
+        border_zone = border_zone.astype(np.int64)
 
         return border_zone
     
     except Exception as e:
-        print(f"Error processing {image_path}: {str(e)} while getting border zone.")
+        print(f"Error processing {image}: {str(e)} while getting border zone.")
         return None
 
-# image_folder = "/mnt/01D9F776C2451850/Carla/labels"
-# Process images
+
+
 image_folder = os.path.join(args.input)
 for filename in os.listdir(image_folder):
     if not filename.endswith("_labels.tif"):
         continue
     print(f"Processing image: {filename}")
-    ventricle = get_ventricle(os.path.join(image_folder, filename))
-    injury = get_injury(os.path.join(image_folder, filename))
-    ventricle_wo_injury = get_ventricle_wo_injury(os.path.join(image_folder, filename))
-    epicardium = get_epicardium(os.path.join(image_folder, filename))
-    border_zone = get_border_zone(os.path.join(image_folder, filename))
+    
+    image = imread(os.path.join(image_folder, filename))
+    
+    ventricle = get_ventricle(image)
+    ventricle_wo_injury = get_ventricle_wo_injury(image)
+    injury = get_injury(ventricle, ventricle_wo_injury)
+    epicardium = get_epicardium(ventricle)
+    border_zone = get_border_zone(injury, ventricle_wo_injury)
     if ventricle is not None:
         tf.imwrite(os.path.join(image_folder, filename.replace("_labels.tif", "_ventricle.tif")), ventricle,compression='zlib')
     if injury is not None:
