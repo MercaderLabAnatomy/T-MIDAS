daaaad2bcf29b8fc96f32debf6b2bacb719444af
Marco Meer <marco.meer@pm.me>
2024-01-23 14:02:14 +0100

    bgr2rgb

--- scripts/czi_to_tif.py
+++ scripts/czi_to_tif.py
@@ -2,7 +2,9 @@ import os
 import argparse
 from aicsimageio import AICSImage
 from aicsimageio.writers import OmeTiffWriter
-
+import numpy as np
+import cv2
+from PIL import Image
 
 # parse arguments
 parser = argparse.ArgumentParser(description='Process czi files.')
@@ -12,21 +14,16 @@ args = parser.parse_args()
 folder = args.input
 
 def czi_scenes_to_tifs(filepath):
-    # test using AICSImageIO
-    aics_img = AICSImage(filepath, reconstruct_mosaic=True)
-    # export each scene as tif
+
+    aics_img = AICSImage(filepath, reconstruct_mosaic=True)    
+
     for i in aics_img.scenes:
         aics_img.set_scene(i)
-        print("Exporting " + str(i) + " with shape "+ str(aics_img.data.shape)+ " and dim order " + str(aics_img.dims.order) + " to ome.tiff.")
-        OmeTiffWriter.save(aics_img.data, 
-                           filepath.replace(".czi", f"_{i}.ome.tiff"), 
-                           physical_pixel_sizes=aics_img.physical_pixel_sizes, 
-                           dim_order=aics_img.dims.order, 
-                           channel_names=aics_img.channel_names,
-                           # list of channel colors RBG hexadecimals
-                           channel_colors=[0xFF0000,0x00FF00,0x0000FF]
-                           )
-        # #aics_img.save(filepath.replace(".czi", f"_{i}.ome.tiff"), select_scenes=[i]) # bug with colors?
+        img = aics_img.get_image_data("YXS", Z=0, T=0, C=0)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        Image.fromarray(img).save(filepath.replace(".czi", f"_{i}.tiff"), 
+                                    dpi = (25.4/aics_img.physical_pixel_sizes[2], 25.4/aics_img.physical_pixel_sizes[1]))
+
 
 for file in os.listdir(folder):
     if file.endswith(".czi"):
