a9400e4908f0a6de2d45a0120518e3b3a6392b34
Marco Meer <marco.meer@pm.me>
2024-01-31 10:47:05 +0100

    changed script

--- scripts/get_myocardium_from_slices.py
+++ scripts/get_myocardium_from_slices.py
@@ -17,19 +17,14 @@ parser = argparse.ArgumentParser(description="Runs automatic mask generation on 
 parser.add_argument("--input", type=str, required=True, help="Path to input images.")
 parser.add_argument("--image_type", type=str, required=True, help="Brightfield images? (y/n)")
 args = parser.parse_args()
-image_folder = os.path.join(args.input)
+image_folder = args.input
 image_type = args.image_type
 
 
-def calculate_threshold(image):
+device = cle.get_device()
+cle.select_device(device.name)
 
-    gray_areas = image[image > 0] # this is to exclude the black background
-    INTENSITY_THRESHOLD = np.percentile(gray_areas, 25) + np.mean(gray_areas)
-
-    return INTENSITY_THRESHOLD
-
-
-def process_image(image_path):
+def process_image(image_path, image_type):
     try:
         if image_type == "y":
             image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -37,15 +32,13 @@ def process_image(image_path):
         else:
             image = imread(image_path)
 
-        INTENSITY_THRESHOLD = calculate_threshold(image)
-        image_to = nsbatwm.threshold_li(image)
-        image_to = label(image_to) # much faster for large images than cle.connected_components_labeling_box 
-        image_to = cle.push(image_to)
-        image_to = cle.exclude_small_labels(image_to,None, 1000)
+        image_to = cle.push(image)
+        image_to = cle.gaussian_blur(image_to, None, 2.0, 2.0, 0.0)
+        image_to = cle.threshold_otsu(image_to)
+        image_to = cle.exclude_small_labels(image_to,None, 1000.0)
         image_labeled = cle.pull(image_to)
         image_labeled = np.array(image_labeled, dtype=np.uint64)
-        # set all nonzero values to 1
-        image_labeled[image_labeled > 0] = 1    
+        image_labeled[image_labeled > 0] = 1    # relabel to 0 and 1
 
         return image_labeled
     except Exception as e:
@@ -56,7 +49,7 @@ for filename in os.listdir(image_folder):
     if not filename.endswith(".tif"):
         continue
     print(f"Processing image: {filename}")
-    labeled_image = process_image(os.path.join(image_folder, filename))
+    labeled_image = process_image(os.path.join(image_folder, filename), image_type)
     if labeled_image is not None:
         tf.imwrite(os.path.join(image_folder, f"{filename[:-4]}_labels.tif"), labeled_image, compression='zlib')
         
