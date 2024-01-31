c1c176b99adad42ee9febbf81189518da2a5a2ac
Marco Meer <marco.meer@pm.me>
2024-01-31 09:52:53 +0100

    get mc from slice, faster and debugged

--- scripts/get_myocardium_from_slices.py
+++ scripts/get_myocardium_from_slices.py
@@ -10,45 +10,33 @@ import tifffile as tf
 import pyclesperanto_prototype as cle
 import napari_segment_blobs_and_things_with_membranes as nsbatwm
 from skimage.measure import label
-
-
+import cv2
 
 # Argument Parsing
 parser = argparse.ArgumentParser(description="Runs automatic mask generation on images.")
 parser.add_argument("--input", type=str, required=True, help="Path to input images.")
 parser.add_argument("--image_type", type=str, required=True, help="Brightfield images? (y/n)")
 args = parser.parse_args()
-
 image_folder = os.path.join(args.input)
 image_type = args.image_type
 
 
-# Define utility functions
-
 def calculate_threshold(image):
-    
-    gray_areas = image[image > 0]
+
+    gray_areas = image[image > 0] # this is to exclude the black background
     INTENSITY_THRESHOLD = np.percentile(gray_areas, 25) + np.mean(gray_areas)
-    
+
     return INTENSITY_THRESHOLD
-
-
-# get label id of largest object using regionprops
-
-def invert_brightfield(image):
-    image = rgb2gray(image)
-    image = np.amax(image) - image # this inverts the image by subtracting the image from the maximum value
-    return image
 
 
 def process_image(image_path):
     try:
-
-        image = imread(image_path)
         if image_type == "y":
-            image = invert_brightfield(image)
-        else :
-            pass
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            image = cv2.bitwise_not(image)
+        else:
+            image = imread(image_path)
+
         INTENSITY_THRESHOLD = calculate_threshold(image)
         image_to = nsbatwm.threshold_li(image)
         image_to = label(image_to) # much faster for large images than cle.connected_components_labeling_box 
@@ -72,5 +60,4 @@ for filename in os.listdir(image_folder):
     if labeled_image is not None:
         tf.imwrite(os.path.join(image_folder, f"{filename[:-4]}_labels.tif"), labeled_image, compression='zlib')
         
-        
-        
+  
