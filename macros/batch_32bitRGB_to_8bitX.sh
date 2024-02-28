5db9e8a27643e50ade5d0811d09155781da526c4
Marco Meer <marco.meer@pm.me>
2024-02-28 14:53:20 +0100

    removed Fiji macros (eventually replaced by Python scripts)

--- macros/batch_32bitRGB_to_8bitX.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter color channel to extract (R, G, or B):"
-read channel 
-
-for file in *.tif; do
-    convert "$file" -channel $channel -depth 8 -separate -set filename:base "%[basename]" "${file%.*}_8bit.tif"
-done
-
