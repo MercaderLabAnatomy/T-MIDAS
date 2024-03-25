196d519e06139002fccfd4a002a165d7b219727c
Marco Meer <marco.meer@pm.me>
2024-03-25 16:07:13 +0100

    testing sparse arrays for coloc

--- scripts/ROI_colocalization_multicolor.py
+++ scripts/ROI_colocalization_multicolor.py
@@ -5,10 +5,10 @@ from skimage import io
 import argparse
 import numpy as np
 import cupy as cp
+from cupyx.scipy.sparse import csr_matrix, coo_matrix
 from cucim.skimage.measure import label, regionprops
 from tifffile import imwrite
 
-# Parse command line arguments
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Script for colocalization analysis of images.')
     parser.add_argument('--input', type=str, help='Path to the parent folder of the channel folders.')
@@ -23,6 +23,7 @@ args = parse_arguments()
 parent_dir = args.input + '/'
 channels = [c.upper() for c in args.channels]
 label_patterns = args.label_patterns
+
 
 # Get a list of files for each channel
 file_lists = {channel: sorted(glob.glob(os.path.join(parent_dir, channel + '/', label_pattern))) for channel, label_pattern in zip(channels, label_patterns)}
@@ -41,44 +42,39 @@ def coloc_channels(file_lists, channels):
         images = {channel: cp.asarray(io.imread(file_lists[channel][i])) for channel in channels}
         
         label_ids = cp.unique(images[channels[0]])
-        if len(label_ids) <= 8:
-            print(f"Unique labels in {channels[0]}: {label_ids}")
-            ROI_masks = {label_id.item(): images[channels[0]] == label_id for label_id in cp.unique(images[channels[0]]) if label_id != 0}
-        else:
-            print(f"Too many unique labels in {channels[0]}: {len(label_ids)}")            
-            ROI_masks = {1: cp.where(images[channels[0]] > 0, 1, 0)} # sets all non-zero pixels to 1
-  
+        ROI_masks = {}
+
+        for label_id in label_ids:
+            ROI_masks[label_id.item()] = csr_matrix(images[channels[0]] == label_id)
         coloc_01 = {label_id: {} for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
         coloc_02 = {label_id: {} for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
         coloc_all = {label_id: {} for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
         unique_labels_01 = {label_id: {} for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
         unique_labels_02 = {label_id: {} for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
         unique_labels_all = {label_id: {} for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}  
-        area = {label_id: regionprops(ROI_masks[label_id].astype(np.int32))[0].area for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
+        area = {label_id: regionprops(ROI_masks[label_id].toarray().astype(np.int32))[0].area for label_id in ROI_masks.keys() if ROI_masks[label_id] is not None}
         
         for label_id in ROI_masks.keys():
-            if ROI_masks[label_id] is not None: 
-                
-                coloc_01[label_id] = ROI_masks[label_id] & (images[channels[1]] > 0)                
+            if ROI_masks[label_id] is not None:              
+                coloc_01[label_id] = (ROI_masks[label_id] != csr_matrix(images[channels[1]] > 0)).toarray()          
                 unique_labels_01[label_id] = cp.max(cp.unique(label(coloc_01[label_id]))).item()
                 
                 if len(channels) == 3:
-                    
-                    coloc_02[label_id] = ROI_masks[label_id] & (images[channels[2]] > 0)
-                    coloc_all[label_id] = ROI_masks[label_id] & (images[channels[1]] > 0) & (images[channels[2]] > 0)
+                    coloc_02[label_id] = (ROI_masks[label_id] != csr_matrix(images[channels[2]] > 0)).toarray()
+                    coloc_all[label_id] = (ROI_masks[label_id] != (csr_matrix((images[channels[1]] > 0) & images[channels[2]] > 0))).toarray()                   
                     unique_labels_02[label_id] = cp.max(cp.unique(label(coloc_02[label_id]))).item()
                     unique_labels_all[label_id] = cp.max(cp.unique(label(coloc_all[label_id]))).item()
         
         filename = os.path.splitext(os.path.basename(file_lists[channels[0]][i]))[0]
         
-        if args.output_images.lower() == 'y':
+        if args.output_images.lower() == 'y' and len(label_ids) <= 8:
             if len(channels) == 2:
                 for label_id in unique_labels_01.keys():
                     coloc_01_image = label(cp.asarray(coloc_01[label_id]))
                     imwrite(parent_dir + f"/{filename}_{channels[1]}_in_{channels[0]}_ROI_{label_id}.tif", 
                             cp.asnumpy(coloc_01_image), compression='zlib')
                     
-            elif len(channels) == 3:
+            elif len(channels) == 3 and len(label_ids) <= 8:
                 for label_id in unique_labels_02.keys():
                     coloc_02_image = label(cp.asarray(coloc_02[label_id]))
                     imwrite(parent_dir + f"/{filename}_{channels[2]}_in_{channels[0]}_ROI_{label_id}.tif", 
@@ -97,12 +93,10 @@ def coloc_channels(file_lists, channels):
             for label_id in unique_labels_all.keys():
                 csv_rows.append([filename, label_id, area[label_id], unique_labels_01[label_id], unique_labels_02[label_id], unique_labels_all[label_id]])
 
-        
         cp.get_default_memory_pool().free_all_blocks()
     
     return csv_rows
 
-# Check the number of channels and perform colocalization analysis accordingly
 csv_rows = coloc_channels(file_lists, channels)
 
 csv_file = parent_dir + '/colocalization.csv'
