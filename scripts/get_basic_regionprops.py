497cb37fb5a4e8de495f5de0c7aa55401bec0ad3
Marco Meer <marco.meer@pm.me>
2024-04-05 09:40:38 +0200

    get basic ROI properties pipeline

--- scripts/get_basic_regionprops.py
+++ scripts/get_basic_regionprops.py
@@ -18,7 +18,6 @@ input_folder = args.input
 
 
 def get_regionprops(tif):
-
     # Read the tif
     img = cp.asarray(io.imread(tif))
 
@@ -28,6 +27,7 @@ def get_regionprops(tif):
     # Get the properties
     df = pd.DataFrame()
     for i, prop in enumerate(props):
+        df.loc[i, 'Filename'] = os.path.basename(tif)
         df.loc[i, 'Label'] = int(prop.label) 
         df.loc[i, 'Area'] = prop.area
         df.loc[i, 'Perimeter'] = prop.perimeter
@@ -38,19 +38,16 @@ def get_regionprops(tif):
     return df
 
 def main():
-
     # Get the tifs
     tifs = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith('_labels.tif')]
 
     # Get the regionprops
     df = pd.DataFrame()
     for tif in tifs:
-        df = pd.concat([df, get_regionprops(tif)])
+        df = pd.concat([df, get_regionprops(tif)]) 
 
     # Save the data in parent folder
     df.to_csv(os.path.join(input_folder, 'regionprops.csv'), index=False)
 
 if __name__ == '__main__':
     main()
-
-
