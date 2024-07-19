3318063a4631637b09d7a4e044e7dc79d2ce4b13
Marco Meer <99955854+macromeer@users.noreply.github.com>
2024-07-19 12:56:18 +0200

    Update install_dependencies.py
    updated installation script

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import os
+import json
+from tqdm import tqdm
 
 def run_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -11,21 +13,31 @@ def run_command(command):
         sys.exit(1)
     return output.decode()
 
+# Get the path to the conda executable
+conda_executable = os.path.join(os.path.dirname(sys.executable), 'conda')
+mamba_executable = os.path.join(os.path.dirname(sys.executable), 'mamba')
+
 env_name = "tmidas-env"
 
-# Create the environment
-run_command(f"conda create -n {env_name} python=3.8 -y")
+# Initialize conda
+print("Initializing conda...")
+run_command(f"{conda_executable} init bash")
 
-# Activate the environment
-activate_command = f"conda activate {env_name}"
-if sys.platform.startswith('win'):
-    activate_command = f"call {activate_command}"
+# Create the environment
+print(f"Creating environment {env_name}...")
+run_command(f"{conda_executable} create -n {env_name} python=3.8 -y")
+
+# Get the path to the created environment
+env_path = run_command(f"{conda_executable} env list --json").strip()
+env_path = json.loads(env_path)['envs']
+env_path = [path for path in env_path if path.endswith(env_name)][0]
 
 # Set up the command prefix to run in the activated environment
-cmd_prefix = f"{activate_command} && "
+cmd_prefix = f"{conda_executable} run -n {env_name} "
 
 # Initialize mamba
-run_command(cmd_prefix + "mamba init")
+print("Initializing mamba...")
+run_command(cmd_prefix + f"{mamba_executable} init")
 
 # Install dependencies
 dependencies = [
@@ -36,16 +48,23 @@ dependencies = [
     'torchvision', 'timm'
 ]
 
+print("Upgrading pip and setuptools...")
 run_command(cmd_prefix + "python -m pip install -U setuptools pip")
 
-run_command(cmd_prefix + "conda install openslide ocl-icd-system pyopencl cupy -y")
+print("Installing conda packages...")
+run_command(cmd_prefix + f"{conda_executable} install openslide ocl-icd-system pyopencl cupy -y")
 
+print("Installing MobileSAM...")
 run_command(cmd_prefix + "pip install git+https://github.com/ChaoningZhang/MobileSAM.git")
 
-for dependency in dependencies:
+print("Installing pip packages...")
+for dependency in tqdm(dependencies, desc="Installing dependencies"):
     run_command(cmd_prefix + f"pip install {dependency}")
 
+print("Installing napari...")
 run_command(cmd_prefix + "python -m pip install napari[all]")
+
+print("Installing cellpose...")
 run_command(cmd_prefix + "python -m pip install cellpose")
 
 print("All dependencies installed successfully.")
