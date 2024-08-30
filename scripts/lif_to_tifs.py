0329ae9ce9fc92bd090d4263972d94a4bbab80de
macromeer <marco.meer@pm.me>
2024-08-30 12:16:39 +0200

    replaed readlif by bioformats

--- scripts/lif_to_tifs.py
+++ scripts/lif_to_tifs.py
@@ -1,116 +1,133 @@
 import os
 import argparse
-import numpy as np
-import cupy as cp
-import tifffile as tf
-from readlif.reader import LifFile
 from tqdm import tqdm
+import javabridge
+import bioformats
+import numpy as np
+import tifffile
+
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process a lif file.')
-    parser.add_argument('--input', type=str, help='path to the lif file')
+    parser.add_argument('--input', type=str, help='path to the folder containing lif files')
     return parser.parse_args()
 
-args = parse_args()
-
-input_folder = args.input
-
-lif_files = []
-for file in os.listdir(input_folder):
-    if file.endswith(".lif"):
-        lif_files.append(file)
-
-
-
-
-
-def scene_to_stack(scene):
-    n_channels = scene.info['channels']
-    array_list = []
-    for i in range(n_channels):
-        # get the array of the current scene
-        frames = []
-        for j in range(scene.nz):
-            for t in range(scene.nt):    
-                frame = scene.get_frame(t=t,c=i,z=j)
-                # convert frame to numpy array
-                frame = cp.array(frame)
-                frames.append(frame)
-        frames = cp.array(frames)
-        array_list.append(frames)
-
-    multichannel_stack = cp.array(array_list)
-    if len(multichannel_stack.shape) < 5:
-         multichannel_stack = cp.expand_dims(multichannel_stack, axis=0)
-    else:
-        pass    
-
-
-    return multichannel_stack
-
-
-def create_metadata(scene):
-    scale_x = scene.info['scale_n'][1]
-    scale_y = scene.info['scale_n'][2]
+def extract_pixel_resolution(lif_file_path):
+    ome_xml_string = bioformats.get_omexml_metadata(path=lif_file_path)
+    ome = bioformats.OMEXML(ome_xml_string)
     
-    # check if scale_z is defined in the dictionary
-    if 3 in scene.info['scale_n']:
-        scale_z = scene.info['scale_n'][3]
-        # if it is NoneType, set it to 0
-        if scale_z is None:
-            scale_z = 0
-
-    else:
-        scale_z = 0
+    resolutions = {}
     
-    # get resolution in um
-    x_res = 1 / scale_x 
-    y_res = 1 / scale_y 
-    z_res = 1 / scale_z if scale_z != 0 else 0
-    resolution = (x_res, y_res)
-    metadata = {'spacing': z_res, 'unit': 'um'}
-    return resolution, metadata
-
-def save_image(image,res_meta,path):
-    # save the image stack
-    tf.imwrite(path, image,resolution = res_meta[0], metadata=res_meta[1], imagej=True, compression='zlib') 
-
-def process_scene(scene,path):
-    multichannel_stack = cp.asnumpy(scene_to_stack(scene))
-    
-    # create metadata
-    res_meta = create_metadata(scene)
-    position = scene.info['name']
-    # replace "/" with "_" in position
-    position = position.replace("/","_")
-    # save the multichannel stack
-    save_image(multichannel_stack,res_meta,path.split(".")[0] +"_{scene}.tif".format(scene=position))
-    print("Processed {scene}".format(scene=position))
-
-for lif_file in tqdm(lif_files, total = len(lif_files), desc="Processing lif files"):
-    
-    if lif_file:
+    for i in range(ome.image_count):
+        image = ome.image(i)
+        pixels = image.Pixels
         
-        path = os.path.join(input_folder, lif_file)
-        # read the lif file
-        lif = LifFile(path)
-        # index all scenes in the lif file
-        img_list = [i for i in lif.get_iter_image()]
+        resolutions[f'image_{i}'] = {
+            'PhysicalSizeX': pixels.PhysicalSizeX,
+            'PhysicalSizeXUnit': pixels.PhysicalSizeXUnit,
+            'PhysicalSizeY': pixels.PhysicalSizeY,
+            'PhysicalSizeYUnit': pixels.PhysicalSizeYUnit,
+            'PhysicalSizeZ': pixels.PhysicalSizeZ,
+            'PhysicalSizeZUnit': pixels.PhysicalSizeZUnit,
+            'SizeX': pixels.SizeX,
+            'SizeY': pixels.SizeY,
+            'SizeZ': pixels.SizeZ
+        }
+    
+    return resolutions
 
-        for scene in img_list:
-            process_scene(scene,path)    
-
-    else:
-        print("No lif file found in the input folder.")
-        exit()
-
-
-
-
-
-
-
-
-
+def safe_float(value, default=0.0):
+    try:
+        return float(value) if value is not None else default
+    except ValueError:
+        return default
 
 
+
+def process_lif_file(lif_file_path, output_directory):
+    try:
+        pixel_resolutions = extract_pixel_resolution(lif_file_path)
+        reader = bioformats.get_image_reader("tmp", path=lif_file_path)
+        series_count = reader.rdr.getSeriesCount()
+
+        for series in range(series_count):
+            reader.rdr.setSeries(series)
+            width = reader.rdr.getSizeX()
+            height = reader.rdr.getSizeY()
+            z_slices = reader.rdr.getSizeZ()
+            channels = reader.rdr.getSizeC()
+            timepoints = reader.rdr.getSizeT()
+            print(f"Processing series {series} dimensions: X={width}, Y={height}, Z={z_slices}, C={channels}, T={timepoints}")
+
+            output_filename = f"series_{series}_C{channels}_Z{z_slices}_T{timepoints}.ome.tiff"
+            output_path = os.path.join(output_directory, output_filename)
+
+            data = np.zeros((timepoints, z_slices, channels, height, width), dtype=np.uint16)
+
+            for t in tqdm(range(timepoints), desc=f"Reading series {series}"):
+                for z in range(z_slices):
+                    for c in range(channels):
+                        plane = reader.read(c=c, z=z, t=t, series=series, rescale=False)
+                        data[t, z, c] = plane
+            
+            resolution = pixel_resolutions[f'image_{series}']
+
+            # Replace 'µm' with 'um' in the unit fields
+            unit = resolution['PhysicalSizeZUnit'] if resolution['PhysicalSizeZUnit'] is not None else 'um'
+            unit = unit.replace('µ', 'u') if 'µ' in unit else unit
+
+            resolution_unit = resolution['PhysicalSizeXUnit'] if resolution['PhysicalSizeXUnit'] is not None else 'um'
+            resolution_unit = resolution_unit.replace('µ', 'u') if 'µ' in resolution_unit else resolution_unit
+
+            # Metadata including resolution
+            metadata = {
+                'axes': 'TZCYX',
+                'channels': channels,
+                'slices': z_slices,
+                'frames': timepoints,
+                'spacing': safe_float(resolution['PhysicalSizeZ']),
+                'unit': unit,
+                'pixel_width': safe_float( resolution['PhysicalSizeX']),
+                'pixel_height': safe_float( resolution['PhysicalSizeY']),
+                'ResolutionUnit': resolution_unit
+            }
+
+            tifffile.imwrite(output_path, data, imagej=True, metadata=metadata, resolution=(1/safe_float(resolution['PhysicalSizeX']), 1/safe_float(resolution['PhysicalSizeY'])))
+
+            print(f"Series {series} saved to: {output_path}")
+
+        reader.close()
+        print(f"Processing complete for {lif_file_path}")
+
+    except Exception as e:
+        print(f"An error occurred while processing {lif_file_path}: {str(e)}")
+        import traceback
+        traceback.print_exc()
+
+
+
+
+
+def main():
+    args = parse_args()
+    input_folder = args.input
+
+    lif_files = [file for file in os.listdir(input_folder) if file.endswith(".lif")]
+
+    try:
+        javabridge.start_vm(class_path=bioformats.JARS, run_headless=True)
+        
+        for lif_file in tqdm(lif_files, total=len(lif_files), desc="Processing lif files"):
+            lif_path = os.path.join(input_folder, lif_file)
+            output_dir = os.path.join(input_folder, lif_file.split(".")[0])
+            os.makedirs(output_dir, exist_ok=True)
+            process_lif_file(lif_path, output_dir)
+
+    except Exception as e:
+        print(f"An error occurred in main: {str(e)}")
+    finally:
+        javabridge.kill_vm()
+
+if __name__ == "__main__":
+    main()
