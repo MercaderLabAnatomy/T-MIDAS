a88f08bfeffb15969f3c2bbc973a03e14748869a
macromeer <marco.meer@pm.me>
2024-09-26 13:42:08 +0200

    replaced imread function

--- scripts/segmentation_semantic_SAM_2D.py
+++ scripts/segmentation_semantic_SAM_2D.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
 import cv2
 import os
 import argparse
-from skimage.io import imread
 import tifffile as tf
 import pyclesperanto_prototype as cle
 from tqdm import tqdm
-# ignore warnings
-import warnings
-warnings.filterwarnings("ignore")
-
+import numpy as np
 from skimage.measure import label
 import torch
 from mobile_sam import sam_model_registry, SamAutomaticMaskGenerator
@@ -22,8 +16,6 @@ Description: This script runs automatic mask generation on 2D images.
 It uses the Mobile-SAM model to generate masks for the input images.
 
 """
-
-
 
 model_type = "vit_t"
 sam_checkpoint = "/opt/T-MIDAS/models/mobile_sam.pt"
@@ -44,15 +36,17 @@ image_folder = args.input
 
 def process_image(image_path):
     try:
-
-        image = imread(image_path)
-        masks = mask_generator.generate(image) # generate masks using Mobile-SAM
+        image = cv2.imread(image_path)
+        
+        masks = mask_generator.generate(image)  # generate masks using Mobile-SAM
+        
+        labels = np.zeros(image.shape[:2], dtype=np.int32)
+        
         for i, mask_data in enumerate(masks):
-              mask = mask_data["segmentation"]
-              labeled_mask = label(mask, return_num=False)
-              labels[labeled_mask > 0] = labeled_mask[labeled_mask > 0] + (i * labeled_mask.max())
-        labels = cle.dilate_labels(labels, None, 1.0)
-        labels = cle.merge_touching_labels(labels)
+            mask = mask_data["segmentation"]
+            labeled_mask = label(mask, return_num=False)
+            labels[labeled_mask > 0] = labeled_mask[labeled_mask > 0] + (i * labeled_mask.max())
+        
         labels = cle.pull(labels)
 
         return labels
@@ -60,10 +54,11 @@ def process_image(image_path):
         print(f"Error processing {image_path}: {str(e)}")
         return None
 
+
 for filename in tqdm(os.listdir(image_folder), total = len(os.listdir(image_folder)), desc="Processing images"):
     if not filename.endswith(".tif"):
         continue
-    #print(f"Processing image: {filename}")
+
     labeled_image = process_image(os.path.join(image_folder, filename))
     if labeled_image is not None:
         tf.imwrite(os.path.join(image_folder, f"{filename[:-4]}_semantic_seg_sam.tif"), labeled_image, compression='zlib')
